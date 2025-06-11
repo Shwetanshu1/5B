00513fceb7129b0cee61006a419b7074395d0635
Shwetanshu1 <shwetanshujha60@gmail.com>
2025-06-11 10:57:44 +0530

    Second commit

---
+++ task1/script.js
@@ -0,0 +1,18 @@
+const head= document.getElementById('head');
+console.log(head)
+
+head.style.color='red';
+
+head.addEventListener('click', ()=>{
+    console.log('h1 element clicked')
+    head.style.backgroundColor='brown'
+})
+const btn= document.getElementById('btn');
+let x= 0;
+btn.addEventListener('click', ()=>{
+    const inner= document.getElementById('inner');
+    x = x+10;
+    if(x<=100){
+          inner.style.width = x+"%";
+    }
+})
